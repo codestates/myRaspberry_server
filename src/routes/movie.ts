b16e29ff411f97f881e7c8725a670d3dc4d6e288
Ubuntu <ubuntu@my-raspberry.asia-northeast3-a.c.united-yeti-296901.internal>
2020-12-15 04:42:11 +0000

    [Hotfix] Modify checking login for get movies

--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -1,9 +1,10 @@
 import * as express from "express";
+import { isLoggedIn } from "./utils";
 import { movieController } from "../controller";
 
 const router = express.Router();
 
 // GET, /movie/updatetag
-router.patch("/updatetag", movieController.updatetag);
+router.patch("/updatetag", isLoggedIn, movieController.updatetag);
 router.get("/:title", movieController.callYoutubeApi);
 export default router;
