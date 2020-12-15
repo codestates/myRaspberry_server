e85a1e4c674d7bcf7b36f3be57838df1f6811954
five1star <five1star0420@gmail.com>
2020-12-15 21:35:12 +0900

    [Task073] fix User entity and swagger

--- src/controller/movie/index.ts
+++ src/controller/movie/index.ts
@@ -1,26 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import User from "../../entity/User";
-import fetch from "node-fetch";
-require('dotenv').config();
+import updatetag from "./updatetag";
+import callYoutubeApi from "./callyoutubeapi";
+import selectmovie from "./selectmovie";
 
-const updatetag = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const id = res.locals.decodedId;
-  const data = { tag: JSON.stringify(req.body) };
-  await User.changeInfo(id, data)
-    .then(() => res.status(200).send("ok"))
-    .catch((err) => next(err));
-};
-
-const callYoutubeApi = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const { title } = req.params;
-    const url: string = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.YOUTUBE_API_KEY}&maxResults=3&type=video&videoEmbeddable=true&q=영화+${title}`;
-    const data: any = await fetch(encodeURI(url)).then(json=>json.json()).catch(err=>console.log(err));
-    res.status(200).send(JSON.stringify(data));
-  } catch (error) {
-    console.log(error);
-    res.status(400).end();
-  }
-}
-
-export { updatetag, callYoutubeApi };
+export { updatetag, callYoutubeApi, selectmovie };
---
+++ src/controller/movie/updatetag.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+import User from "../../entity/User";
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const id = res.locals.decodedId;
+  const data = { tag: JSON.stringify(req.body) };
+  await User.changeInfo(id, data)
+    .then(() => res.status(200).send("ok"))
+    .catch((err) => next(err));
+};
