be5a1d5c26dc3d9980c33c77f6caa4a83f199090
five1star <five1star0420@gmail.com>
2021-01-04 14:05:56 +0900

    [Task091] redisign route and controller

--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -4,8 +4,6 @@ import { movieController } from "../controller";
 
 const router = express.Router();
 
-// PATCH, /movie/updatetag
-router.patch("/updatetag", isLoggedIn, movieController.updatetag);
 // PATCH, /movie/selectmovie
 router.patch("/selectmovie/", isLoggedIn, movieController.selectmovie);
 // GET, /movie/selectmovie
